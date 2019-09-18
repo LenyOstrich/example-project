5399320783269ea9f49c383fd34764b132c058e7
LenyOstrich <leny_ostrich@mail.ru>
2019-09-18 16:31:05 +0300

    second commit

--- frontend/src/main/java/com/exampleproject/gwt/startpoint/client/StartPoint.java
+++ frontend/src/main/java/com/exampleproject/gwt/startpoint/client/StartPoint.java
@@ -31,6 +31,27 @@ public class StartPoint implements EntryPoint {
 
         Defaults.setServiceRoot(GWT.getHostPageBaseURL() + "backend");
 
+        Button checkServer = new Button("Check Server");
+        checkServer.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                client.gett(new MethodCallback<String>() {
+                    @Override
+                    public void onFailure(Method method, Throwable exception) {
+                        // TODO Auto-generated method stub
+                        Window.alert(exception.toString() + "\n" + exception.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(Method method, String response) {
+                        // TODO Auto-generated method stub
+                        Window.alert(response);
+                    }
+                });
+            }
+        });
+        RootPanel.get().add(checkServer);
+
         Button send = new Button("Send");
         send.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
--- frontend/src/main/java/com/exampleproject/gwt/startpoint/client/WorkerClient.java
+++ frontend/src/main/java/com/exampleproject/gwt/startpoint/client/WorkerClient.java
@@ -9,12 +9,16 @@ import org.fusesource.restygwt.client.RestService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-
+@Path("http://localhost:8091")
 public interface WorkerClient extends RestService {
 
 	@GET
-	@Path("http://localhost:8091/test")
+	@Path("/test1")
 	void get(MethodCallback<String> callback);
+
+	@GET
+	@Path("/test")
+	void gett(MethodCallback<String> callback);
 
 	@GET
 	@Path("/testDb")
